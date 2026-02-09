1b6ed51c7048711ed4aef10118fb31beef71dc44
Madhav2005-Garg <madhavgarg545@gmail.com>
2026-02-09 09:53:11 +0530

    adding the code of the servo motor in main.cpp and also initialising the libraries and the servo motor pin

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo myServo;
 void setup() {
 // write your initialization code here
+
+    myServo.attach(9);
+    Serial.begin(9600);
 }
 
 void loop() {
 // write your code here
+    for (int pos=0;pos<=180;pos+=1) {
+        myServo.write(pos);
+        delay(15);
+        Serial.print("Servo Position: ");
+        Serial.println(pos);
+    }
+        for (int pos=180;pos>=0;pos-=1) {
+            myServo.write(pos);
+            delay(15);
+            Serial.print("Servo Position: ");
+            Serial.println(pos);
+        }
 }
